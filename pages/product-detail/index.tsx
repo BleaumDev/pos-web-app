abe8c26aa8aae1836e97dc629f2beb035f7bcee1
Musab Gulfam <musabgulfam0722@gmail.com>
2023-10-09 20:28:06 +0500

    Done with Product Detail UI & Product Add UI along with responsiveness

--- pages/product-detail/index.tsx
+++ pages/product-detail/index.tsx
@@ -6,6 +6,14 @@ import {
   Input,
   Select,
   Textarea,
+  Text,
+  Table,
+  Th,
+  Thead,
+  Tr,
+  Td,
+  Tbody,
+  TableContainer,
 } from '@chakra-ui/react';
 import Slider from 'react-slick';
 
@@ -115,6 +123,145 @@ export default function ProductDetail(): React.ReactNode {
                 borderWidth={2}
                 className="primary-font-medium"
               />
+            </div>
+          </div>
+          <div className="mt-7">
+            <Heading
+              h={4}
+              className="primary-font-semi-bold-italic"
+              size={'md'}
+              color={'#41454B'}
+            >
+              Complex Pricing
+            </Heading>
+            <div
+              className="rounded-3xl p-6 border-black mt-8 w-full"
+              style={{
+                borderWidth: 1,
+              }}
+            >
+              <TableContainer>
+                <Table variant="unstyled" size={'sm'} autoCapitalize="none">
+                  <Thead
+                    className="mx-3"
+                    style={{
+                      borderColor: '#41454B',
+                      borderBottomWidth: 1,
+                    }}
+                  >
+                    <Tr>
+                      <Th
+                        h={6}
+                        className="primary-font-semibold"
+                        fontSize={'md'}
+                        color={'#41454B'}
+                        style={{
+                          textTransform: 'none',
+                        }}
+                      >
+                        Weight
+                      </Th>
+                      <Th
+                        className="primary-font-semibold"
+                        fontSize={'md'}
+                        color={'#41454B'}
+                        style={{
+                          textTransform: 'none',
+                        }}
+                      >
+                        Price
+                      </Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody className="mt-5">
+                    <Tr>
+                      <Td className="primary-font-medium" fontSize={'sm'}>
+                        1g
+                      </Td>
+                      <Td>
+                        <div
+                          className="rounded-lg px-6 py-2 flex justify-center items-center text-white max-w-min"
+                          style={{
+                            backgroundColor: '#FFA382',
+                          }}
+                        >
+                          <Text className="primary-font-medium" fontSize={'sm'}>
+                            $ 3.00
+                          </Text>
+                        </div>
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td className="primary-font-medium" fontSize={'sm'}>
+                        3.5g
+                      </Td>
+                      <Td>
+                        <div
+                          className="rounded-lg px-6 py-2 flex justify-center items-center text-white max-w-min"
+                          style={{
+                            backgroundColor: '#FFA382',
+                          }}
+                        >
+                          <Text className="primary-font-medium" fontSize={'sm'}>
+                            $ 5.00
+                          </Text>
+                        </div>
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td className="primary-font-medium" fontSize={'sm'}>
+                        7g
+                      </Td>
+                      <Td>
+                        <div
+                          className="rounded-lg px-6 py-2 flex justify-center items-center text-white max-w-min"
+                          style={{
+                            backgroundColor: '#FFA382',
+                          }}
+                        >
+                          <Text className="primary-font-medium" fontSize={'sm'}>
+                            $ 8.00
+                          </Text>
+                        </div>
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td className="primary-font-medium" fontSize={'sm'}>
+                        14g
+                      </Td>
+                      <Td>
+                        <div
+                          className="rounded-lg px-6 py-2 flex justify-center items-center text-white max-w-min"
+                          style={{
+                            backgroundColor: '#FFA382',
+                          }}
+                        >
+                          <Text className="primary-font-medium" fontSize={'sm'}>
+                            $ 21.00
+                          </Text>
+                        </div>
+                      </Td>
+                    </Tr>
+                    <Tr>
+                      <Td className="primary-font-medium" fontSize={'sm'}>
+                        28g
+                      </Td>
+                      <Td>
+                        <div
+                          className="rounded-lg px-6 py-2 flex justify-center items-center text-white max-w-min min-w-min"
+                          style={{
+                            backgroundColor: '#FFA382',
+                          }}
+                        >
+                          <Text className="primary-font-medium" fontSize={'sm'}>
+                            $ 40.00
+                          </Text>
+                        </div>
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
             </div>
           </div>
         </div>
